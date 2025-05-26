5b8badc15d558fe6013c1d803beda996de65b881
sayu171 <tyutyo2979@icloud.com>
2025-05-26 18:57:58 +0900

    jsを実装

---
+++ script.js
@@ -0,0 +1,11 @@
+const increment = document.getElementsByTagName("button")[0];
+const decrement = document.getElementsByTagName("button")[1];
+const display = document.getElementsByTagName("h2")[0];
+ 
+increment.addEventListener("click", (e) => {
+    display.textContent = Number(display.textContent) + 1;
+});
+ 
+decrement.addEventListener("click", (e) => {
+    display.textContent = Number(display.textContent) - 1;
+});
